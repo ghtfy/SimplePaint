825e648b5ed430ae2ae3eb0ab641567466897d7a
keunwoo kim <keun1999@outlook.kr>
2026-04-30 12:02:42 +0900

    과제 2 코딩과 테스트 완료 마우스 드래그를 이용하여 직선, 사각형,원 그리기 기능을 구현하였습니다. 선 두께를 조절하여 도형의 선 굵기를 조절하도록 하였습니다.

--- Form1.cs
+++ Form1.cs
@@ -3,18 +3,169 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 
 namespace SimplePaint
 {
     public partial class Form1 : Form
     {
+        enum ToolType { Line, Rectangle, Circle }  // 사용할 도형 타입
+        private Bitmap canvasBitmap;          // 실제 그림이 저장되는 비트맵
+        private Graphics canvasGraphics;      // 비트맵위에 그리기 위한 객체
+        private bool isDrawing= false;       // 현재 드래그중인지 여부
+        private Point startPoint;             // 드래그 시작점
+        private Point endPoint;               // 드래그 끝점
+        private ToolType currentTool= ToolType.Line;  // 현재 선택된 도형
+        private Color currentColor= Color.Black;      // 현재 색상
+        private int currentLineWidth= 2;              // 현재 선 두께
+
         public Form1()
         {
             InitializeComponent();
+
+            canvasBitmap = new Bitmap(picCanvas.Width, picCanvas.Height); 
+            canvasGraphics = Graphics.FromImage(canvasBitmap); 
+            canvasGraphics.Clear(Color.White);   // 캔버스를 흰색으로 초기화
+
+            picCanvas.Image = canvasBitmap;   // 그린 그림을 화면(PictureBox)에표시
+
+            // 마우스이벤트연결
+            picCanvas.MouseDown+= PicCanvas_MouseDown;
+            picCanvas.MouseMove+= PicCanvas_MouseMove;
+            picCanvas.MouseUp+= PicCanvas_MouseUp;
+
+            // picCanvas가 다시 그려질때 PicCanvas_Paint함수를 실행하도록 연결
+            picCanvas.Paint+= PicCanvas_Paint;
+            
+            // 도형 선택 버튼 이벤트연결
+            btnLine.Click+= btnLine_Click;
+            btnRectangle.Click+= btnRectangle_Click;
+            btnCircle.Click+= btnCircle_Click;
+
+            // 색상 콤보 박스 이벤트 연결
+            cmbColor.SelectedIndexChanged += cmbColor_SelectedIndexChanged;
+            cmbColor.SelectedIndex= 0;  // 기본값: Black
+
+            // 선 두께 트랙바 이벤트 연결
+            trbLineWidth.Minimum= 1;    // 최소값
+            trbLineWidth.Maximum= 10;   // 최대값
+            trbLineWidth.Value= 2;
+            trbLineWidth.ValueChanged+= trbLineWidth_ValueChanged;
+        }
+
+        private void PicCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;             // 드래그시작
+            startPoint= e.Location;      // 시작점저장
+         }
+        private void PicCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isDrawing) return;       // 그림그리기와상관없는마우스움직임은무시
+            endPoint= e.Location;        // 현재위치갱신
+            
+            // picCanvas를다시그려라(Paint 이벤트를발생시킨다)
+            picCanvas.Invalidate();       // 화면다시그리기(미리보기)
+              }
+
+        private void PicCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!isDrawing) return;     // 그림그리기와상관없는마우스움직임은무시
+            isDrawing= false;          // 드래그종료
+            endPoint= e.Location;
+            // 실제비트맵에도형그리기(확정)
+
+            using (Pen pen= new Pen(currentColor, currentLineWidth))
+            {
+                DrawShape(canvasGraphics, pen, startPoint, endPoint);
+            }
+            picCanvas.Invalidate();     // 다시그려서결과반영, Paint 이벤트발생
+        }
+
+        private void PicCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (!isDrawing) return;
+            
+            // 점선펜(미리보기용)
+            using (Pen previewPen= new Pen(currentColor, currentLineWidth))
+            {
+                previewPen.DashStyle= DashStyle.Dash;
+                DrawShape(e.Graphics, previewPen, startPoint, endPoint);
+            }
+        }
+
+        private void DrawShape(Graphics g, Pen pen, Point p1, Point p2) 
+        { 
+            Rectangle rect = GetRectangle(p1, p2);
+            switch (currentTool)
+            { 
+                case ToolType.Line:
+                    g.DrawLine(pen, p1, p2); 
+                    break;
+                case ToolType.Rectangle:
+                    g.DrawRectangle(pen, rect); 
+                    break;
+                case ToolType.Circle:
+                    g.DrawEllipse(pen, rect); 
+                    break; 
+            } 
+        }
+
+        private Rectangle GetRectangle(Point p1, Point p2) 
+        {
+            return new Rectangle
+                (Math.Min(p1.X, p2.X), 
+                Math.Min(p1.Y, p2.Y),
+                Math.Abs(p1.X - p2.X),
+                Math.Abs(p1.Y - p2.Y)
+                ); 
+        }
+
+        private void btnLine_Click(object sender, EventArgs e) 
+        { 
+            currentTool = ToolType.Line; 
+        }
+        private void btnRectangle_Click(object sender, EventArgs e) 
+        {
+            currentTool = ToolType.Rectangle; 
+        }
+        private void btnCircle_Click(object sender, EventArgs e) 
+        {
+            currentTool = ToolType.Circle;
+        }
+
+        private void cmbColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbColor.SelectedIndex)
+            {
+                case 0: // Black 검정
+                currentColor= Color.Black;
+                    break;
+                case 1: // Red 빨강
+                currentColor= Color.Red;
+                    break;
+                case 2: // Blue 파랑
+                currentColor= Color.Blue;
+                    break;
+                case 3: // Green 녹색
+                currentColor= Color.Green;
+                    break;
+                default:
+                    currentColor= Color.Black;
+                    break;
+            }
+        }
+
+        private void trbLineWidth_ValueChanged(object sender, EventArgs e) 
+        {
+            currentLineWidth = trbLineWidth.Value;
         }
 
         private void Form1_Load(object sender, EventArgs e)
